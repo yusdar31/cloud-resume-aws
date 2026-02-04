68edbc6cd59746f903cfac7c0484898f5b6564df
Andi Yusdar Al Imran <andi.yusdar@mastersystem.co.id>
2026-02-04 15:15:28 +0700

    Add terraform outputs for cloudfront cdn

--- outputs.tf
+++ outputs.tf
@@ -5,3 +5,8 @@ output "website_url" {
 output "api_endpoint" {
   value = aws_lambda_function_url.api_url.function_url
 }
+
+output "cloudfront_distribution_id" {
+  description = "ID dari CloudFront Distribution"
+  value       = aws_cloudfront_distribution.s3_distribution.id
+}
